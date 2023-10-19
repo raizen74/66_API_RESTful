fd41f8a7cd99b116c9f59dac0115ec98f36acfdd
raizen74 <raizen74@gmail.com>
2023-10-19 19:50:11 +0200

    Add Files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cafe & Wifi API RESTful
+Build a REST API Service to make HTTP requests to a Coffee Shops SQLite Database. Requests allowed:
+
+- GET a random coffee shop.
+- GET all coffee shops.
+- Search all shops at a particular location.
+- POST a new shop (insert to the database).
+- PATCH coffee price at a particular shop, given its database id.
+- DELETE a coffee shop from the database.
+
+**Cafe & Wifi** API RESTful [documentation](https://www.kaggle.com/competitions/competicioiimludg2023).
---
+++ main.py
@@ -0,0 +1,142 @@
+import random
+
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+    
+    def to_dict(self):
+        #Method 1. 
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            #Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+        
+        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+with app.app_context(): #create_contect
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+## HTTP GET - Read Record
+@app.route("/random", methods=["GET"])  # GET is allowed by default on all routes, not need to be specified.
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    print(random_cafe)
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
+
+@app.route("/all")
+def get_all_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    # all_cafes = {random_cafe.id:{
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # } for random_cafe in all_cafes}
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+@app.route("/search")
+def search_cafe():
+    query_location = request.args.get("loc") #/search?loc=Peckham
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
+    all_cafes = result.scalars().all()
+    if all_cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
+## HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+## HTTP PUT/PATCH - Update Record
+@app.route('/update-price/<int:cafe_id>', methods=["PATCH"])
+def get_user(cafe_id):
+    if cafe:=db.session.get(Cafe, cafe_id):
+        cafe.coffee_price = request.args.get("new_price")
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found in the database."}), 404
+
+## HTTP DELETE - Delete Record
+@app.route('/report-closed/<cafe_id>', methods = ['DELETE'])
+def delete(cafe_id):
+    if not (cafe:=db.session.get(Cafe, cafe_id)):
+        return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found in the database."}), 404
+    elif request.args.get("api-key") != 'TopSecretApiKey':
+        return jsonify(error={"error": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+    db.session.delete(cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Cafe successfully deleted."}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
